ce34e9fd52a4775e59f26cd49afc8b9cdc529497
Petermuturi <pmuturi65@gmail.com>
2015-07-22 11:34:35 +0300

    Drinks poster

---
+++ app/src/main/java/moja.java
@@ -0,0 +1,17 @@
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.alienske.chillspot.R;
+
+/**
+ * Created by User on 7/21/2015.
+ */
+public class moja extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.moja);
+    }
+
+
+}
